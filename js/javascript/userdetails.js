ae1c8848f4d645b329fa13b043d222ef3b505861
Thiyagarajan03 <thiyagarajan.rajendran03@gmail.com>
2021-08-15 14:03:56 +0530

    walletadded

---
+++ js/javascript/userdetails.js
@@ -0,0 +1,25 @@
+function userDetails(){
+    let userStr = localStorage.getItem("LOGGED_IN_USER");
+    let user = userStr != null ? JSON.parse(userStr):null;
+    console.log(user)
+    let content =`<h1>${user.balance}</h1>`;
+        document.querySelector("#userName").innerHTML = content;
+}
+userDetails()
+
+function addMoney(){
+    let userStr = localStorage.getItem("LOGGED_IN_USER");
+    let user = userStr != null ? JSON.parse(userStr):null;
+    console.log(user)
+    const money=document.querySelector("#addmoney").value
+    if(money==0){
+        alert("enter the amount")
+    }else{
+        let Obj={
+            "amount":money
+        }
+        const url=`https://product-mock-api.herokuapp.com/rechargeapp/api/v1/users/${user.id}/wallet`;
+        axios.patch(url,Obj).then(res=>console.log(res.data))
+    }
+    
+}
