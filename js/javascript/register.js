a6a34b5347af92352be5a4071140bd0c7f458e42
Thiyagarajan03 <thiyagarajan.rajendran03@gmail.com>
2021-08-19 11:57:06 +0530

    apiadded

--- js/javascript/register.js
+++ js/javascript/register.js
@@ -1,40 +1,33 @@
-function register(){
-    event.preventDefault() 
-   const name= document.querySelector("#name").value;
-   const email= document.querySelector("#email").value;
-   const password= document.querySelector("#password").value;
-   if(password.length<8)
-   {
-       alert ("password must be atleast 8 digits");
-       return false;
-   }
-   else if(name==null||name==""||name.trim()=="")
-   {
-       alert("name cannot be empty");
-       return false;
-   }
-   else
-   {
-      
-    const registerObj={
-        "name":name,
-        "email":email,
-        "password":password
-
-    };
-    console.log(registerObj);
-    const userName="apikey-v2-kf8ex4frj52lu2wwin72qqktpi3occ9bfv4p80vbr99";
-    const dbpassword="68fc5b9dc8c58071087abaecc44a5f29";
-    const basicAuth = 'Basic ' + btoa(userName + ':' + dbpassword);
-    const url="https://2fbcb9ec-d57d-431a-8d72-186d88ddf478-bluemix.cloudantnosqldb.appdomain.cloud/rechargeapp_users"
-    axios.post(url,obj,{ headers: {'Authorization': basicAuth }}).then(res => {
-        console.log(res.data);
-        alert("Successfully registered");
-        window.location.href="login.html";
+function register() {
+    event.preventDefault()
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    if (password.length < 8) {
+        alert("password must be atleast 8 digits");
+        return false;
     }
-    ).catch(err => {
+    else if (name == null || name == "" || name.trim() == "") {
+        alert("name cannot be empty");
+        return false;
+    }
+    else {
 
-        alert("Unable to register");
-    })
-}
+        const registerObj = {
+            "name": name,
+            "email": email,
+            "password": password
+
+        };
+        console.log(registerObj);
+        UserService.register(registerObj).then(res => {
+            console.log(res.data);
+            alert("Successfully registered");
+            window.location.href = "login.html";
+        }
+        ).catch(err => {
+
+            alert("Unable to register");
+        })
+    }
 }
---
+++ js/service.js
@@ -0,0 +1,25 @@
+class UserService {
+    static login(userName, password) {
+        const selectedData = {
+            selector: {
+                email: email,
+                password: password
+            },
+            fields: ["_id", "name", "email"]
+        };
+        const dbUserName = "apikey-v2-kf8ex4frj52lu2wwin72qqktpi3occ9bfv4p80vbr99";
+        const dbPassword = "68fc5b9dc8c58071087abaecc44a5f29";
+        const basicAuth = 'Basic ' + btoa(dbUserName + ':' + dbPassword);
+        let url = "https://2fbcb9ec-d57d-431a-8d72-186d88ddf478-bluemix.cloudantnosqldb.appdomain.cloud/rechargeapp_users/_find";
+        return (axios.post(url, selectedData, { headers: { 'Authorization': basicAuth } }));
+    }
+    static register(registerObj) {
+
+        const dbUserName = "apikey-v2-kf8ex4frj52lu2wwin72qqktpi3occ9bfv4p80vbr99";
+        const dbPassword = "68fc5b9dc8c58071087abaecc44a5f29";
+        const basicAuth = 'Basic ' + btoa(dbUserName + ':' + dbPassword);
+
+        let url = "https://2fbcb9ec-d57d-431a-8d72-186d88ddf478-bluemix.cloudantnosqldb.appdomain.cloud/rechargeapp_users";
+        return (axios.post(url, registerObj, { headers: { 'Authorization': basicAuth } }));
+    }
+}
