f2e26f4714a54448a5529f2a991b7c2f7f6d2634
Thiyagarajan03 <thiyagarajan.rajendran03@gmail.com>
2021-08-27 14:53:54 +0530

    userhistoryadded

--- js/javascript/userdetails.js
+++ js/javascript/userdetails.js
@@ -1,31 +1,114 @@
-function userDetails(){
+function userDetails() {
     let userStr = localStorage.getItem("LOGGED_IN_USER");
-    let user = userStr != null ? JSON.parse(userStr):null;
-    console.log(user)
-    let content =`<h1>`+"Balance  : "+`${user.balance}</h1>`;
-        document.querySelector("#userName").innerHTML = content;
+    let user = userStr != null ? JSON.parse(userStr) : null;
+    console.log(user);
 }
 userDetails()
 
-function addMoney(){
+function addMoney() {
     let userStr = localStorage.getItem("LOGGED_IN_USER");
-    let user = userStr != null ? JSON.parse(userStr):null;
+    let user = userStr != null ? JSON.parse(userStr) : null;
     console.log(user)
-    const money=document.querySelector("#addmoney").value
-    
-    updatedData = {
-        'name' : user.name,
-        'email' : user.email,
-        'password': user.password,
-        'balance' : parseInt(user.balance) + parseInt(money)
-    }
-    UserService.update(updatedData, user._id, user._rev).then(res=>{
-        console.log("updated Status : ",res.data);
-        alert("Added To wallet");
-    }).catch(err=>{
-        alert("Faild To load Money")
-    })
-    
-}
+    const money = document.querySelector("#addmoney").value
 
+
+    UserService.userDetails(user._id).then(res => {
+        console.log(res.data)
+        const data = res.data
+        updatedData = {
+            'name': data.name,
+            'email': data.email,
+            'password': data.password,
+            'balance': parseInt(data.balance) + parseInt(money)
+        }
+        console.log(data)
+        UserService.update(updatedData, data._id, data._rev).then(res => {
+            localStorage.setItem("LOGGED_IN_USER", JSON.stringify(data));
+            console.log(data);
+            console.log("updated Status : ", res.data);
+            alert("Added To wallet");
+            window.location.href = 'wallet.html'
+        }).catch(err => {
+            alert("Faild To load Money")
+        })
+
+
+    });
+
+}
+function reduceMoney() {
+    let userStr = localStorage.getItem("LOGGED_IN_USER");
+    let user = userStr != null ? JSON.parse(userStr) : null;
+    console.log(user);
+    const amount = document.querySelector("#paymentType").value;
+    const queryString = window.location.search;
+    console.log(queryString);
+    const urlParams = new URLSearchParams(queryString);
+    const price = urlParams.get('price');
+    let _price = "";
+    for (let i = 3; i < price.length; i++) {
+        _price += price[i];
+    }
+    UserService.userDetails(user._id).then(res => {
+        console.log(res.data);
+        const data = res.data
+        balance = {
+            'name': data.name,
+            'email': data.email,
+            'password': data.password,
+            'balance': parseInt(data.balance) - parseInt(_price)
+        }
+        console.log(balance)
+        UserService.data(balance, data._id, data._rev).then(res => {
+            console.log("updated Status : ", res.data);
+            console.log(res.data)
+            alert("Amount debited successfully");
+            historyUpdate();
+        }).catch(err => {
+            alert("Faild To load Money")
+        })
+    }).catch(err => console.error(err.message))
+
+
+
+}
+function historyUpdate() {
+    let userStr = localStorage.getItem("LOGGED_IN_USER");
+    let user = userStr != null ? JSON.parse(userStr) : null;
+    console.log(user.email);
+    const queryString = window.location.search;
+    console.log(queryString);
+    const urlParams = new URLSearchParams(queryString);
+    const simName = urlParams.get('provider');
+    const price = urlParams.get('price');
+    const validity = urlParams.get('validity');
+    const number = urlParams.get('mobilenumber');
+    
+    let date = new Date();
+    const historyObj = {
+        "email": user.email,
+        "provider": simName,
+        "price": price,
+        "phoneNumber": number,
+        "validity": validity,
+        "rechargeDate": date
+    }
+    UserService.history(historyObj).then(res=>{
+        alert("History created");
+    }).catch(err=>{
+        alert("Failed to create History");
+    })
+}
+function walletBalance() {
+
+    let userStr = localStorage.getItem("LOGGED_IN_USER");
+    let user = userStr != null ? JSON.parse(userStr) : null;
+    console.log(user._id)
+    UserService.userDetails(user._id).then(res => {
+        console.log(res.data);
+        let content = `<h1>` + "Balance  : " + `${res.data.balance}</h1>`;
+        document.querySelector("#userName").innerHTML = content;
+    }).catch(err => console.error(err.message))
+}
+walletBalance();
 
